3a02a939e4b51655f4f28821372cb117d4260111
khoaHyh <khoahuynh.toronto@gmail.com>
2020-11-23 15:08:52 -0500

    test deploy

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,7 +8,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
           {'Smart-brain will detect faces in your pictures. Give it a try.'}
       </p>
       <p>
-          {"(Smart-brain won't work with data/image(base64) types of images.)"}
+          {"(Smart-brain won't work with data/image(base64) types of images)"}
       </p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
